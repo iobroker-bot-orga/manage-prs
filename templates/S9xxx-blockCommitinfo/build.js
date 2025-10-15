b92b420fb1a4a5973304364ea29be65e1ec255c6
Martin M. <mcm57@gmx.at>
2025-10-15 18:06:00 +0200

    rename template

--- templates/S9xxx-blockCommitinfo/build.js
+++ templates/S9xxx-blockCommitinfo/build.js
@@ -1,22 +1,23 @@
 const fs = require("node:fs");
+const path = require("node:path");
 
 const fileName = './.gitignore';
 
 if (!fs.existsSync(fileName)) {
     console.log(`❌ ${fileName} does not exist, cannot create PR.`);
-    process.exit(0);
+    process.exit (0);
 }
 
-console.log(`✓ ${fileName} exists.`);
+console.log( `✓ ${fileName} exists.`);
 
 let fileContent = fs.readFileSync(fileName, 'utf8');
 if (fileContent.includes('.commitinfo')) {
-    console.log(`✔️ ${fileName} already contains ".commitinfo", no need for a PR.`);
+    console.log( `✓ ${fileName} already contains ".commitinfo", no need for a PR.`);
     process.exit(0);
 }
 
 fs.appendFileSync(fileName, '\n#ignore .commitinfo created by ioBroker release script\n.commitinfo\n');
-console.log(`✔️ .commitinfo appended to ${fileName}. PR will be created`);
+console.log( `✓ .commitinfo appended to ${fileName}. PR will be created`);
 
 process.exit(0);
 
